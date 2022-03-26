6f8527eadb50401fb1b6eb65c547f557c5e32f86
Adhiraj16a12 <64204448+Adhiraj16a12@users.noreply.github.com>
2022-03-26 22:24:12 +0530

    Add files via upload

---
+++ play-on-click.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent("play-on-click", {
+  init: function() {
+    this.onClick = this.onClick.bind(this);
+  },
+  play: function() {
+    window.addEventListener("click", this.onClick);
+  },
+  onClick: function(evt) {
+    var videoEl = this.el.getAttribute("material").src;
+    if (!videoEl) {
+      return;
+    }
+    this.el.object3D.visible = true;
+    videoEl.play();
+  }
+});
